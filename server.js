41c7a56c120003fbd03b70e5541b842948bef8ba
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-01-23 02:51:25 +0000

    refactor: fetch environment variables

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
 const app = express();
 
+require("dotenv").config();
+
 const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
 
 mongoose.connect(MONGODB_CONNECTION_URL, {
